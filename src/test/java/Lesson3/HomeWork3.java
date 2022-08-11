ff1064d051ec93006709a75ee5ca86fb12ce572e
kirill <kirillgolan@mail.ru>
2022-08-12 00:30:31 +0300

    HomeWork3 is done

---
+++ src/test/java/Lesson3/HomeWork3.java
@@ -0,0 +1,229 @@
+package Lesson3;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+
+public class HomeWork3 extends AbstractTest {
+
+    @BeforeAll
+    static void setUp(){
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    }
+
+    @Test
+    void italianFoodGet(){
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "italian")
+                .when()
+                .get("https://api.spoonacular.com/recipes/complexSearch")
+                //.prettyPeek()
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void italianVegetarianGet() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "italian")
+                .queryParam("diet", "vegetarian")
+                .when()
+                .get("https://api.spoonacular.com/recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void koreanGlutenFreeGet() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "korean")
+                .queryParam("diet", "gluten free")
+                .when()
+                .get("https://api.spoonacular.com/recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void thaiVeganGet() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "thai")
+                .queryParam("diet", "vegan")
+                .when()
+                .get("https://api.spoonacular.com/recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void italianKetogenicGet() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "italian")
+                .queryParam("diet", "ketogenic")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void chickenSoupPost() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title", "chiken soup")
+                .formParam("ingredient list", "chicken water")
+                .when()
+                .post(getBaseUrl() + "recipes/cuisine")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void cheeseSoupPost() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title", "cheese soup")
+                .formParam("ingredient list", "cheese water")
+                .when()
+                .post(getBaseUrl() + "recipes/cuisine")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void porkSoupPost() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title", "pork soup")
+                .formParam("ingredient list", "pork water")
+                .when()
+                .post(getBaseUrl() + "recipes/cuisine")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void horseSoupPost() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title", "horse soup")
+                .formParam("ingredient list", "horse water")
+                .when()
+                .post(getBaseUrl() + "recipes/cuisine")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+    @Test
+    void crabSoupPost() {
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title", "crab soup")
+                .formParam("ingredient list", "crab water")
+                .when()
+                .post(getBaseUrl() + "recipes/cuisine")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine(containsString("OK"))
+                .header("Connection", "keep-alive")
+                .header("Content-Length", Integer::parseInt, lessThan(3000))
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+    }
+
+}
+
